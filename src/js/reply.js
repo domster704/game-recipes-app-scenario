c57afff173bdda17596582ba6096e4449e65986f
domster704 <domster704@mail.ru>
2024-05-30 23:34:29 +0300

    fix tabs

--- src/js/reply.js
+++ src/js/reply.js
@@ -1,14 +1,14 @@
-function reply(body, response){
+function reply(body, response) {
     var replyData = {
         type: "raw",
         body: body
-    };    
+    };
     response.replies = response.replies || [];
     response.replies.push(replyData);
 }
 
 
-function addAction(action, context){
+function addAction(action, context) {
     var command = {
         type: "smart_app_data",
         action: action
@@ -22,15 +22,15 @@ function addAction(action, context){
             return;
         }
     }
-    
+
     return reply({items: [{command: command}]}, context.response);
 }
 
 
 function addSuggestions(suggestions, context) {
     var buttons = [];
-    
-    suggestions.forEach (function(suggest) {
+
+    suggestions.forEach(function (suggest) {
         buttons.push(
             {
                 action: {
